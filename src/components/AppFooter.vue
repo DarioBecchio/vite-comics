275c216c91c3477c0559705f84e5549b16932c6b
Dario Becchio <dabek1986@gmail.com>
2024-02-07 19:09:08 +0100

    create cards

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,7 +4,7 @@ export default {
 };
 </script>
 <template>
-  <div class="resume">
+  <div class="resume container">
     <div class="footermenu">
       <div class="linkmenu">
         <ul class="leftmenu">
@@ -48,11 +48,37 @@ export default {
       </div>
     </div>
   </div>
-  <div class="followmenu">
-    <div class="signup"></div>
+  <div class="bottommenu container">
+    <div class="signup">
+      <button>SIGN-UP NOW</button>
+    </div>
+    <div class="followusmenu">
+      <ul>
+        <li>
+          <a href="#"><img src="./img/footer-facebook.png" alt="" /></a>
+        </li>
+        <li>
+          <a href="#"><img src="./img/footer-twitter.png" alt="" /></a>
+        </li>
+        <li>
+          <a href="#"><img src="./img/footer-youtube.png" alt="" /></a>
+        </li>
+        <li>
+          <a href="#"><img src="./img/footer-pinterest.png" alt="" /></a>
+        </li>
+        <li>
+          <a href="#"><img src="./img/footer-periscope.png" alt="" /></a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <style>
+.container {
+  width: 70%;
+  max-width: 1176px;
+  margin: auto;
+}
 .resume {
   background-image: url("./img/footer-bg.jpg");
 }
@@ -64,6 +90,12 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.followmenu {
+.bottommenu {
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--dc-lgDark);
+}
+.followusmenu {
+  display: flex;
 }
 </style>
---
+++ src/components/ComicCards.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "ComicCards",
+  props: {
+    card: Object,
+  },
+};
+</script>
+<template>
+  <div class="cards">
+    <img :src="card.thumb" alt="" />
+    <h3>{{ card.series }}</h3>
+    <p>{{ card.type }}</p>
+    <p>{{ card.price }}</p>
+  </div>
+</template>
+<style></style>
